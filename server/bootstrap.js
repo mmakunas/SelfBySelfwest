128e83eb878f79a0f07130af5b487b73e673768e
Michael Makunas <michael.makunas@viacom.com>
2014-12-06 10:50:32 -0500

    final tweaks

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -7,8 +7,8 @@ Meteor.startup(function () {
     Twit = new TwitMaker({
         consumer_key: 'aG6lZyEqVTkEnDSAAwpBSCjOJ'
         , consumer_secret: 'xi5m3CaJREFPtP2dHjrQSY9eZEXsxOSfv4ryt7EhiXKBcKAF0l'
-        , access_token: '2919941173-CYHC23YrJ0CKC9Dr7vG3AjmICC1xQYYjC9XVs2N'
-        , access_token_secret: 'Bu4e9pboaLn7k89ZwGiRFYz6lFFKEsiuGSHPWGTMcE1m2'
+        , access_token: '2919941173-SBq4nYhXMaNgX6vdrjELazrV6W6aPXSCmwEkdex'
+        , access_token_secret: 'ani3mypWASbnrpDM6yaCZTJno7DXtAphL148hoXs6JPrG'
     });
 
     Twit.getSync = Meteor.wrapAsync(Twit.get.bind(Twit));
@@ -16,11 +16,8 @@ Meteor.startup(function () {
 
     var saveTopTweet = function (id, tweet) {
         TopTweets.update(id,
-        {"$set":{"name":tweet.user.name}}, {upsert: true});
-
-        console.log("incrementing...: " + id);
+        {"$set":{"name":tweet.user.screen_name}}, {upsert: true});
         TopTweets.update({_id: id}, { "$inc" : { "score" : Math.floor(Random.fraction() * 10) * Math.floor(Random.fraction() * 10) } });
-
 
     }
 
@@ -40,12 +37,18 @@ Meteor.startup(function () {
              })
              */
             var tweet = Twit.getSync('statuses/show/:id', {id: name})
+            //var oembed = Twit.getSync('statuses/oembed.json?:id', {id: name})
 
             saveTopTweet(name, tweet)
 
 
         });
     }, 15000)
+
+   // Twit.get('statuses/oembed.json?:id', { id: 540974974430105601 }, function (err, data, response) {
+   //     console.log(data);
+   //     console.log(err);
+   // })
 
 
 
